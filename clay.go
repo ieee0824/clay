800cf34394fdeeac97c09bd29885b1c095b8bc7a
ieee0824 <ieee0824@ezweb.ne.jp>
2017-09-19 11:50:11 +0900

    convertMap test

--- clay.go
+++ clay.go
@@ -1,1 +1,54 @@
 package clay
+
+import (
+	"reflect"
+	"errors"
+	"fmt"
+)
+
+var ConvertMap = convertMap
+
+func mergeMap(src, dst map[string]interface{}) {
+	for k, v := range dst {
+		src[k] = v
+	}
+}
+
+func convertMap(i interface{}, path string) (map[string]interface{}, error) {
+	t := reflect.TypeOf(i)
+
+	switch t.Kind() {
+	case reflect.Map:
+		ret := map[string]interface{}{}
+		m, ok := i.(map[string]interface{})
+		if !ok {
+			return nil, errors.New("")
+		}
+
+		for k, v := range m {
+			result, err := convertMap(v, fmt.Sprintf("%s/%s", path, k))
+			if err != nil {
+				return nil, err
+			}
+			mergeMap(ret, result)
+		}
+		return ret, nil
+
+	case reflect.Slice:
+		s, ok := i.([]string)
+		if !ok {
+			return nil, errors.New("")
+		}
+		return map[string] interface{} {
+			path: s,
+		}, nil
+	default:
+		s, ok := i.(string)
+		if !ok {
+			return nil, errors.New("")
+		}
+		return map[string]interface{} {
+			path: s,
+		}, nil
+	}
+}
---
+++ convertMap_test.go
@@ -0,0 +1,47 @@
+package clay
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestConvertMap(t *testing.T) {
+	tests := []struct {
+		input map[string]interface{}
+		want map[string]interface{}
+		err bool
+	}{
+		{
+			input:map[string]interface{}{
+				"slice": []string{"foo", "bar", "baz"},
+				"map": map[string]interface{}{
+					"country": "japan",
+					"llustrator": []string{
+						"カントク",
+						"鈴平ひろ",
+					},
+				},
+			},
+			want: map[string]interface{}{
+				"/slice": []string{"foo", "bar", "baz"},
+				"/map/country:japan": map[string]interface{}{
+					"/map/llustrator": []string{"カントク","鈴平ひろ"},
+				},
+			},
+			err: false,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := convertMap(test.input, "")
+		if !test.err && err != nil {
+			t.Fatalf("should not be error for %v but: %v", test.input, err)
+		}
+		if test.err && err == nil {
+			t.Fatalf("should be error for %v but not:", test.input)
+		}
+		if reflect.DeepEqual(got, test.want) {
+			t.Fatalf("want %q, but %q:", test.want, got)
+		}
+	}
+}
